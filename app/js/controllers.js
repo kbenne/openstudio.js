c87d85e49cb496b76ffb5cbbe1484b024961288a
Kyle Benne <kyle.benne@nrel.gov>
2015-07-01 22:50:46 -0500

    Start to draw systems

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -39,3 +39,50 @@ var zonesController = function ($scope) {
   };
 };
 openstudioApp.controller('zonesController', zonesController);
+
+var systemsController = function ($scope) {
+
+  osshapes = {
+    standardUnit: 100.0,
+
+    getShape: function(modelObject) {
+      result = {};
+      if( openstudio.model.toNode(modelObject).isNull() ) {
+        result = new osshapes.StraightComponent();
+      } else {
+        result = new osshapes.Node();
+      };
+      return result;
+    },
+  
+    StraightComponent: function() {
+      shape = new createjs.Shape();
+      shape.graphics.beginStroke("black").moveTo(0.0,0.0).lineTo(osshapes.standardUnit,0.0);
+      return shape;
+    },
+
+    Node: function() {
+      shape = new createjs.Shape();
+      shape.graphics.beginStroke("black").moveTo(0.0,0.0).lineTo(osshapes.standardUnit,0.0);
+      shape.graphics.beginFill("black").drawCircle(osshapes.standardUnit / 2.0,0,7.0);
+      return shape;
+    }
+  };
+
+  openstudio.model.addSystemType7(model);
+  plant = openstudio.model.getPlantLoops(model).get(0);
+  supplyComps = plant.supplyComponents();
+
+  stage = new createjs.Stage("demoCanvas");
+
+  for( i = 0; i < supplyComps.size(); ++i ) {
+    shape = osshapes.getShape(supplyComps.get(i));
+    shape.x = i * (osshapes.standardUnit + 10.0);
+    shape.y = 50.0;
+    stage.addChild(shape);
+  };
+
+  stage.update();
+};
+openstudioApp.controller('systemsController', systemsController);
+
