e1e7735441810bb6e143b17709d882ce5c3c3ce5
Kyle Benne <kyle.benne@nrel.gov>
2015-07-21 08:37:28 -0500

    Add mixer and branch drop zone

--- app/controllers/SystemsCtrl.js
+++ app/controllers/SystemsCtrl.js
@@ -23,8 +23,9 @@ openstudioApp.controller('SystemsCtrl', ['$scope', '$log', 'os', function ($scop
 
     drawNode: function() {
       var shape = new createjs.Shape();
-      shape.graphics.beginStroke("black").moveTo(0.0,0.0).lineTo(osshapes.standardUnit,0.0);
-      shape.graphics.beginFill("black").drawCircle(osshapes.standardUnit / 2.0,0,7.0);
+      shape.graphics.beginStroke("black").moveTo(0.0,0.0).lineTo(osshapes.standardUnit / 2.0 - 7.0,0.0);
+      shape.graphics.beginStroke("black").drawCircle(osshapes.standardUnit / 2.0,0,7.0);
+      shape.graphics.beginStroke("black").moveTo(osshapes.standardUnit / 2.0 + 7.0,0.0).lineTo(osshapes.standardUnit,0.0);
       return shape;
     },
 
@@ -34,6 +35,23 @@ openstudioApp.controller('SystemsCtrl', ['$scope', '$log', 'os', function ($scop
       for( var i = 0; i < branchLengths; ++i ) {
         shape.graphics.beginStroke("black").moveTo(osshapes.standardUnit / 2.0,osshapes.standardUnit * i).lineTo(osshapes.standardUnit,osshapes.standardUnit * i);
       };
+      return shape;
+    },
+
+    drawMixer: function(branchLengths) {
+      var shape = new createjs.Shape();
+      shape.graphics.beginStroke("black").moveTo(osshapes.standardUnit / 2.0,0.0).lineTo(osshapes.standardUnit / 2.0,osshapes.standardUnit * branchLengths);
+      for( var i = 0; i < branchLengths; ++i ) {
+        shape.graphics.beginStroke("black").moveTo(0.0,osshapes.standardUnit * i).lineTo(osshapes.standardUnit / 2.0,osshapes.standardUnit * i);
+      };
+      return shape;
+    },
+
+    drawDropZone: function() {
+      var offset = 15;
+      var shape = new createjs.Shape();
+      shape.graphics.beginStroke("black").drawRoundRect(0.0,offset - osshapes.standardUnit / 2.0,osshapes.standardUnit * 2,osshapes.standardUnit - offset * 2,5.0);
+      shape.setBounds(0.0,offset - osshapes.standardUnit / 2.0,osshapes.standardUnit * 2,osshapes.standardUnit - offset * 2);
       return shape;
     }
   };
@@ -54,42 +72,60 @@ openstudioApp.controller('SystemsCtrl', ['$scope', '$log', 'os', function ($scop
   const plant = os.openstudio.model.getPlantLoops(os.model).get(0);
   const supplyComps = plant.supplyComponents();
 
-  var splitter = false;
+  var splitterModelObject = false;
   for( var i = 0; i != supplyComps.size(); ++i ) {
     const option = os.openstudio.model.toSplitter(supplyComps.get(i));
-    if( ! option.isNull() ) splitter = option.get();
+    if( ! option.isNull() ) splitterModelObject = option.get();
   };
 
-  var mixer = false;
+  var mixerModelObject = false;
   for( var i = 0; i != supplyComps.size(); ++i ) {
     const option = os.openstudio.model.toMixer(supplyComps.get(i))
-    if( ! option.isNull() ) mixer = option.get();
+    if( ! option.isNull() ) mixerModelObject = option.get();
   };
 
   var stage = new createjs.Stage("demoCanvas");
 
-  var xpos = 0.0;
+  var xpos = osshapes.standardUnit;
+  var ypos = 0.5 + (osshapes.standardUnit);
 
-  if( splitter && mixer ) {
-    var outletObjects = splitter.outletModelObjects();
-    var inletObjects = mixer.inletModelObjects();
-
-    xpos = xpos + osshapes.standardUnit;
+  if( splitterModelObject && mixerModelObject ) {
+    var outletObjects = splitterModelObject.outletModelObjects();
+    var inletObjects = mixerModelObject.inletModelObjects();
 
     var splitter = osshapes.drawSplitter(outletObjects.size());
     splitter.x = xpos;
-    splitter.y = 0.5 + (osshapes.standardUnit);
+    splitter.y = ypos;
     stage.addChild(splitter);
+
+    var branches = new Array;
+    var longestBranchLength = 0;
+    for( var i = 0; i < inletObjects.size(); ++i ) {
+      var branchComps = plant.supplyComponents(os.openstudio.model.toHVACComponent(outletObjects.get(i)).get(),os.openstudio.model.toHVACComponent(inletObjects.get(i)).get());
+      if( branchComps.size() > longestBranchLength ) { longestBranchLength = branchComps.size(); }
+      branches[i] = oscontainers.drawBranch(branchComps);
+    };
 
     xpos = xpos + osshapes.standardUnit;
 
     for( var i = 0; i < inletObjects.size(); ++i ) {
-      var branchComps = plant.supplyComponents(os.openstudio.model.toHVACComponent(outletObjects.get(i)).get(),os.openstudio.model.toHVACComponent(inletObjects.get(i)).get());
-      var branch = oscontainers.drawBranch(branchComps);
+      var branch = branches[i];
       branch.x = xpos;
-      branch.y = 0.5 + (i + 1) * osshapes.standardUnit;
+      branch.y = ypos + i * osshapes.standardUnit;
       stage.addChild(branch);
     };
+
+    var dropZone = osshapes.drawDropZone();
+    dropZone.x = xpos + (longestBranchLength / 2.0 * osshapes.standardUnit) - (dropZone.getBounds().width / 2.0);
+    dropZone.y = ypos + inletObjects.size() * osshapes.standardUnit;
+    stage.addChild(dropZone);
+
+    xpos = xpos + (longestBranchLength * osshapes.standardUnit);
+
+    var mixer = osshapes.drawMixer(outletObjects.size());
+    mixer.x = xpos;
+    mixer.y = ypos;
+    stage.addChild(mixer);
   };
 
   stage.update();
